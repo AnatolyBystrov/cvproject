71d601c4b35f1df4aa835cb912c2051eb10f3be1
Anatoly <anatolyavbme@gmail.com>
2025-04-18 18:17:33 +0300

    🔥 Add CORS-friendly OPTIONS route for /generate

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PDFController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::post('/generate', [PDFController::class, 'generate']);
+
+Route::options('/generate', function (Request $request) {
+    return response('', 204)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
